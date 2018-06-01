74e063ec34370ecf93ad582c441340376414cdc3
mohamad abdallah <cr7_mohamad@hotmail.com>
2018-06-01 23:37:53 +0300

    p

---
+++ myutils/src/main/java/mhd/com/myutils/MyMath.java
@@ -0,0 +1,18 @@
+package mhd.com.myutils;
+
+/**
+ * Created by User on 6/1/2018.
+ */
+
+public class MyMath {
+
+    public static int Plus(int a,int b){
+
+        return a+b;
+    }
+
+    public static int Minus(int a ,int b){
+
+        return a-b;
+    }
+}
